766b9efca60b92f558c657311388d9abbd27fe58
Malkan <malkanshaheen45@gmail.com>
2025-08-01 12:44:07 +0500

    updated frame and timer

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,5 +1,93 @@
-import React from 'react';
-import './Hero.css'; // Import the CSS
+import React, { useState, useEffect } from 'react';
+import './Hero.css';
+
+const AnimatedCard = ({ animation, digit }) => {
+  return (
+    <div className={`flipCard ${animation}`}>
+      <span>{digit}</span>
+    </div>
+  );
+};
+
+const StaticCard = ({ position, digit }) => {
+  return (
+    <div className={position}>
+      <span>{digit}</span>
+    </div>
+  );
+};
+
+const FlipUnitContainer = ({ digit, shuffle, unit }) => {
+  let currentDigit = digit;
+  let previousDigit = digit - 1;
+
+  if (unit !== 'hours') {
+    previousDigit = previousDigit === -1 ? 59 : previousDigit;
+  } else {
+    previousDigit = previousDigit === -1 ? 23 : previousDigit;
+  }
+
+  if (currentDigit < 10) {
+    currentDigit = `0${currentDigit}`;
+  }
+  if (previousDigit < 10) {
+    previousDigit = `0${previousDigit}`;
+  }
+
+  const digit1 = shuffle ? previousDigit : currentDigit;
+  const digit2 = !shuffle ? previousDigit : currentDigit;
+
+  const animation1 = shuffle ? 'fold' : 'unfold';
+  const animation2 = !shuffle ? 'fold' : 'unfold';
+
+  return (
+    <div className={'flipUnitContainer'}>
+      <StaticCard position={'upperCard'} digit={currentDigit} />
+      <StaticCard position={'lowerCard'} digit={previousDigit} />
+      <AnimatedCard digit={digit1} animation={animation1} />
+      <AnimatedCard digit={digit2} animation={animation2} />
+    </div>
+  );
+};
+
+const FlipClock = () => {
+  const [time, setTime] = useState({
+    hours: 0,
+    hoursShuffle: true,
+    minutes: 0,
+    minutesShuffle: true,
+    seconds: 0,
+    secondsShuffle: true,
+  });
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      const date = new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+
+      setTime(prevTime => ({
+        hours,
+        hoursShuffle: hours !== prevTime.hours ? !prevTime.hoursShuffle : prevTime.hoursShuffle,
+        minutes,
+        minutesShuffle: minutes !== prevTime.minutes ? !prevTime.minutesShuffle : prevTime.minutesShuffle,
+        seconds,
+        secondsShuffle: seconds !== prevTime.seconds ? !prevTime.secondsShuffle : prevTime.secondsShuffle,
+      }));
+    }, 50);
+
+    return () => clearInterval(timerID);
+  }, []);
+
+  return (
+    <div className={'flipClock'}>
+      <FlipUnitContainer unit={'hours'} digit={time.hours} shuffle={time.hoursShuffle} />
+      <FlipUnitContainer unit={'minutes'} digit={time.minutes} shuffle={time.minutesShuffle} />
+      <FlipUnitContainer unit={'seconds'} digit={time.seconds} shuffle={time.secondsShuffle} />
+    </div>
+  );
+};
 
 const ProjectBlackLanding = () => {
   return (
@@ -20,6 +108,7 @@ const ProjectBlackLanding = () => {
         <button className="cta-button">Get Updates</button>
         <div className="countdown-wrapper">
           <p className="coming-soon">Coming Soon (30 August 2025)</p>
+          <FlipClock />
         </div>
       </div>
    </div>
