013dcfb933b5df272b63e12bb293b67dc6c5a345
Malkan <malkanshaheen45@gmail.com>
2025-08-05 23:10:38 +0500

    updated responsiveness

--- src/vision/vision.js
+++ src/vision/vision.js
@@ -26,23 +26,29 @@ const Vision = () => {
           <img src={img4} alt="Visual 4" className={`frame-img frame-img-4 slide-up delay-3`} />
         </div>
 
-        <div className="text-wrapper">
-          <div className={`empower-text ${imagesAnimated ? 'fade-in-text' : ''}`}>
-            <h2 className="empower-heading">The Vision: Empower Black Excellence Together</h2>
-            <p className="empower-desc">
-              Despite contributing $1.6 trillion annually to the U.S. economy, Black creators and businesses remain underrepresented. Project Black is here to change that.
-            </p>
-            <p className="empower-desc">
-              We’re building a digital platform where culture, commerce, and community unite to shift narratives, multiply opportunity, and make Black excellence the rule—not the exception.
-              <br />
-              Built for us. Backed by us. This is how we redefine the standard—together.
-            </p>
-          </div>
+ <div className="text-wrapper">
+  <div className={`empower-text ${imagesAnimated ? 'fade-in-text' : ''}`}>
+    <div className="empower-heading-wrapper">
+      <h2 className="empower-heading">The Vision: Empower Black Excellence Together</h2>
+    </div>
 
-          <div className="line line-top-left"></div>
-          <div className="line line-bottom-left"></div>
-          <div className="line line-right-vertical"></div>
-        </div>
+    <div className="empower-desc-wrapper">
+      <p className="empower-desc">
+        Despite contributing $1.6 trillion annually to the U.S. economy, Black creators and businesses remain underrepresented. Project Black is here to change that.
+      </p>
+      <p className="empower-desc">
+        We’re building a digital platform where culture, commerce, and community unite to shift narratives, multiply opportunity, and make Black excellence the rule—not the exception.
+        <br />
+        Built for us. Backed by us. This is how we redefine the standard—together.
+      </p>
+    </div>
+  </div>
+
+  <div className="line line-top-left"></div>
+  <div className="line line-bottom-left"></div>
+  <div className="line line-right-vertical"></div>
+</div>
+
       </section>
     </div>
   );
